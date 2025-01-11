a2305345ded6b72ab7fc8aa12109b0f4a260d066
Derrick Chea <trackmeetapp@outlook.com>
2025-01-11 16:38:04 -0500

    Add files via upload

---
+++ Solidity Lesson 1.cpp
@@ -0,0 +1,15 @@
+ //SPDX-License-Identifier: MIT
+ pragma solidity 0.8.12; //remember to make commentns in your code 
+
+contract SimpleStorage{
+// SOlity datav type 
+//boolean, unit, int, address, bytes
+
+bool hasFavoriteNumber = true;
+
+
+uint256 favoriteNumber = 5;
+string favoriteNumberInText = "5";
+int256 favoriteInt = 5;
+address myAdress = 0x9F63610C8B0eed1Be88E148858dff6FbF27D6394;
+bytes32 favoriteBytes = "cats";
